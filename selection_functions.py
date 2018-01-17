658ec0ab5aae94f17ac8a96ef5dfe7b2e609e216
davidrpugh <drobert.pugh@gmail.com>
2018-01-17 05:20:18 +0300

    Added Wright-Bergstrom selection function.

--- selection_functions.py
+++ selection_functions.py
@@ -1,5 +1,16 @@
+"""
+Module containing various selection functions previously used in the literature.
+
+@author davidrpugh
+
+"""
 def kirkpatrick_selection(x_A, d=1):
     return d * x_A / (1 + (d - 1) * x_A)
 
+
 def seger_selection(x_A, d=0):
     return x_A * (1 + d * (1 - x_A))
+
+
+def wright_bergstrom_matching(x_A, d=0):
+    return d + (1 - d) * xA
