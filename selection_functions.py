27b680679201702cad35d394d6a52f72fafaeec8
davidrpugh <drobert.pugh@gmail.com>
2018-01-19 15:33:42 +0300

    Added derivs.

--- selection_functions.py
+++ selection_functions.py
@@ -8,8 +8,16 @@ def kirkpatrick_selection(x_A, d=1):
     return d * x_A / (1 + (d - 1) * x_A)
 
 
+def kirkpatrick_selection_derivative(x_A, d=1):
+    return (1 + (d - 1) * x_A)**-2
+
+
 def seger_selection(x_A, d=0):
     return x_A * (1 + d * (1 - x_A))
+
+
+def seger_selection_derivative(x_A, d=0):
+    return 1 + d * (1 - 2 * x_A)
 
 
 def wright_bergstrom_matching(x_A, d=0):
