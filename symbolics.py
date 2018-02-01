da26049dfa352dc0a560aefa802bebad87f24456
davidrpugh <drobert.pugh@gmail.com>
2018-02-01 07:46:32 +0300

    Simulations now using the symbolic model

--- symbolics.py
+++ symbolics.py
@@ -6,6 +6,8 @@ class U(sym.Function):
 
     is_real = True
 
+    is_positive = True
+
     @classmethod
     def eval(cls, x):
         """We require the U(0)=0 and U(1)=1"""
@@ -16,18 +18,18 @@ class U(sym.Function):
 
 
 class UGA(U):
-    """Matching function for G males."""
+    """Matching function for G females."""
 
 
 class UgA(U):
-    """Matching function for g males."""
+    """Matching function for g females."""
 
 
 x1, x2, x3 = sym.symbols('x1, x2, x3')
 T, R, P, S = sym.symbols('T, R, P, S')
 
 
-def N(x1, x2, x3, UGA, UgA, T, R, P, S):
+def total_offspring(x1, x2, x3, UGA, UgA, T, R, P, S):
     out = (
            2 * R *((x1 + x2) * UGA(x1 + x3)**2 + (1 - (x1 + x2)) * UgA(x1 + x3)**2) +
            2 * P * ((x1 + x2) * (1 - UGA(x1 + x3))**2 + (1 - (x1 + x2)) * (1 - UgA(x1 + x3))**2) +
@@ -71,7 +73,7 @@ def equation_motion_GA_share(x1, x2, x3, UGA, UgA, T, R, P, S):
                  (1 - x1 - x2 - x3) * 2 * UgA(x1 + x3) * (1 - UgA(x1 + x3)) * (1/4) * x1 / (x1 + x3) * S # 0
                  )
 
-    x1_dot = (numerator / N(x1, x2, x3, UGA, UgA, T, R, P, S)) - x1
+    x1_dot = (numerator / total_offspring(x1, x2, x3, UGA, UgA, T, R, P, S)) - x1
     return x1_dot
 
 def equation_motion_Ga_share(x1, x2, x3, UGA, UgA, T, R, P, S):
@@ -110,7 +112,7 @@ def equation_motion_Ga_share(x1, x2, x3, UGA, UgA, T, R, P, S):
 
                  )
 
-    x2_dot = (numerator / N(x1, x2, x3, UGA, UgA, T, R, P, S)) - x2
+    x2_dot = (numerator / total_offspring(x1, x2, x3, UGA, UgA, T, R, P, S)) - x2
     return x2_dot
 
 
@@ -151,7 +153,7 @@ def equation_motion_gA_share(x1, x2, x3, UGA, UgA, T, R, P, S):
 
                  )
 
-    x3_dot = (numerator / N(x1, x2, x3, UGA, UgA, T, R, P, S)) - x3
+    x3_dot = (numerator / total_offspring(x1, x2, x3, UGA, UgA, T, R, P, S)) - x3
     return x3_dot
 
 
@@ -190,18 +192,29 @@ def equation_motion_ga_share(x1, x2, x3, UGA, UgA, T, R, P, S):
                  (1 - x1 - x2 - x3) * 2 * UgA(x1 + x3) * (1 - UgA(x1 + x3)) * (1) * (1 - x1 - x2 - x3) / (1 - x1 - x3) * T
                  )
 
-    x4_dot = (numerator / N(x1, x2, x3, UGA, UgA, T, R, P, S)) - (1 - x1 - x2 - x3)
+    x4_dot = (numerator / total_offspring(x1, x2, x3, UGA, UgA, T, R, P, S)) - (1 - x1 - x2 - x3)
     return x4_dot
 
 
 F = sym.Matrix([equation_motion_GA_share(x1, x2, x3, UGA, UgA, T, R, P, S),
                 equation_motion_Ga_share(x1, x2, x3, UGA, UgA, T, R, P, S),
-                equation_motion_gA_share(x1, x2, x3, UGA, UgA, T, R, P, S)])
+                equation_motion_gA_share(x1, x2, x3, UGA, UgA, T, R, P, S),
+                equation_motion_ga_share(x1, x2, x3, UGA, UgA, T, R, P, S)])
+
+_F = sym.lambdify((x1, x2, x3, UGA, UgA, T, R, P, S), F, modules="numpy")
 
 
-#_F = sym.lambdify((x, T, R, P, S), F, modules="numpy")
+# equations for the monomorphic_gamma model
+f = sym.Matrix([equation_motion_GA_share(x1, x2, 0, UGA, UgA, T, R, P, S),
+                equation_motion_Ga_share(x1, x2, 0, UGA, UgA, T, R, P, S)])
+_f = sym.lambdify((x1, x2, UGA, UgA, T, R, P, S), f, modules="numpy")
 
 
 def generalized_sexual_selection(x, UGA, UgA, payoff_kernel):
-    T, R, P, S = payoff_kernel
-    return _F(x, UGA, UgA, T, R, P, S)
+    ((R, S), (T, P)) = payoff_kernel
+    return _F(x[0], x[1], x[2], UGA, UgA, T, R, P, S)
+
+
+def monomorphic_gamma_sexual_selection(x, UGA, UgA, payoff_kernel):
+    ((R, S), (T, P)) = payoff_kernel
+    return _f(x[0], x[1], UGA, UgA, T, R, P, S)
