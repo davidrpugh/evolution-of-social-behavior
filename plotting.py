af04b33c8dbe963614beb7be4659a7515caeca67
davidrpugh <drobert.pugh@gmail.com>
2018-02-02 08:18:56 +0300

    Saved new plotting methods.

--- plotting.py
+++ plotting.py
@@ -6,7 +6,7 @@ Module containing various plotting functions.
 """
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import integrate
+from scipy import integrate, optimize
 
 import models
 import selection_functions
@@ -16,22 +16,23 @@ import symbolics
 def plot_generalized_sexual_selection(x1, x2, x3, selection_function, d1, d3,
                                       T, R, P, S, max_time):
 
-    fig, ax = plt.subplots(1,1, figsize=(10,8))
+    fig, axes = plt.subplots(1,2, figsize=(15,8), sharex=True)
 
     # prepare the axes
-    ax.set_ylim((0, 1))
-    ax.set_xlabel(r"Time, $t$", fontsize=15)
-    ax.set_ylabel(r"Offspring genotype shares, $x_i$", fontsize=15)
+    axes[0].set_ylim((0, 1))
+    axes[0].set_xlabel(r"Time, $t$", fontsize=15)
+    axes[0].set_ylabel(r"Offspring genotype shares, $x_i$", fontsize=15)
+
+    axes[1].set_xlabel(r"Time, $t$", fontsize=15)
+    axes[1].set_ylabel(r"Total Offspring (Fitness), $N(t)$", fontsize=15)
 
     # create the initial condition
-    x4 = 1 - (x1 + x2 + x3)
-    y0=np.array([x1,x2,x3,x4])
-    assert np.allclose(y0.sum(), 1)
+    y0=np.array([x1,x2,x3])
+    assert y0.sum() <= 1
 
     # create the payoff kernel
     assert (T > R) and (R > P) and (R > S), "Payoffs must satisfy either Prisoner's Dilemma or Stag Hunt constraints! T={}, R={}, P={}, S={}".format(T, R, P, S)
-    payoff_kernel = np.array([[R, S],
-                              [T, P]])
+    payoff_kernel = np.array([[R, S], [T, P]])
 
     # create the selection functions
     if selection_function == "kirkpatrick":
@@ -40,22 +41,37 @@ def plot_generalized_sexual_selection(x1, x2, x3, selection_function, d1, d3,
     elif selection_function == "seger":
         UGA = lambda x_A: selection_functions.seger_selection(x_A, d1)
         UgA = lambda x_A: selection_functions.seger_selection(x_A, d3)
+    elif selection_function == "wright-bergstrom":
+        UGA = lambda x_A: selection_functions.wright_bergstrom_selection(x_A, d1)
+        UgA = lambda x_A: selection_functions.wright_bergstrom_selection(x_A, d3)
     else:
-      raise ValueError("selection_function must be one of \"kirkpatrick\" or \"seger\".")
+        valid_funcs = ("kirkpatrick", "seger", "wright-bergstrom")
+        msg = "Selection_function must be one of {}, {}, or {}.".format(*valid_funcs)
+        raise ValueError(msg)
 
     # simulate the model starting from a random initial condition
     f = lambda t, y: symbolics.generalized_sexual_selection(y, UGA, UgA, payoff_kernel)
-    result = integrate.solve_ivp(f, t_span=(0, max_time), y0=y0, rtol=1e-9, atol=1e-12,
-                                 dense_output=True, vectorized=True)
+    solution = integrate.solve_ivp(f, t_span=(0, max_time), y0=y0, rtol=1e-9,
+                                   atol=1e-12, dense_output=True, vectorized=True)
 
-    ax.plot(result.t, result.y[0], label="GA")
-    ax.plot(result.t, result.y[1], label="Ga")
-    ax.plot(result.t, result.y[2], label="gA")
-    ax.plot(result.t, result.y[3], label="ga")
-    ax.legend()
+    axes[0].plot(solution.t, solution.y[0], label="GA")
+    axes[0].plot(solution.t, solution.y[1], label="Ga")
+    axes[0].plot(solution.t, solution.y[2], label="gA")
+    axes[0].plot(solution.t, 1 - solution.y.sum(axis=0), label="ga")
+    axes[0].legend()
+
+    total_offspring = lambda y: symbolics.total_offspring(y[0], y[1], y[2], UGA, UgA, T, R, P, S)
+    axes[1].plot(solution.t, total_offspring(solution.y))
+
+    f = lambda x: -symbolics._equilibrium_total_offspring(x[0], x[1], x[2], T, R, P, S)
+    x0 = 0.5 * np.ones(3)
+    optimize_result = optimize.minimize(f, x0, bounds=[(0,1), (0,1), (0,1)])
+    axes[1].axhline(-optimize_result.fun, color='k', linestyle="--", label=r"$\bar{N}^*$")
+    axes[1].legend()
+    
     plt.show()
 
-    return result
+    return (solution, optimize_result)
 
 
 def plot_monomorphic_gamma_sexual_selection(x1, selection_function, d1, d3,
@@ -85,8 +101,13 @@ def plot_monomorphic_gamma_sexual_selection(x1, selection_function, d1, d3,
     elif selection_function == "seger":
         UGA = lambda x_A: selection_functions.seger_selection(x_A, d1)
         UgA = lambda x_A: selection_functions.seger_selection(x_A, d3)
+    elif selection_function == "wright-bergstrom":
+        UGA = lambda x_A: selection_functions.wright_bergstrom_selection(x_A, d1)
+        UgA = lambda x_A: selection_functions.wright_bergstrom_selection(x_A, d3)
     else:
-      raise ValueError("selection_function must be one of \"kirkpatrick\" or \"seger\".")
+        valid_funcs = ("kirkpatrick", "seger", "wright-bergstrom")
+        msg = "Selection_function must be one of {}, {}, or {}.".format(*valid_funcs)
+        raise ValueError(msg)
 
     # simulate the model starting from a random initial condition
     f = lambda t, y: symbolics.monomorphic_gamma_sexual_selection(y, UGA, UgA, payoff_kernel)
--- symbolics.py
+++ symbolics.py
@@ -25,10 +25,6 @@ class UgA(U):
     """Matching function for g females."""
 
 
-x1, x2, x3 = sym.symbols('x1, x2, x3')
-T, R, P, S = sym.symbols('T, R, P, S')
-
-
 def total_offspring(x1, x2, x3, UGA, UgA, T, R, P, S):
     out = (
            2 * R *((x1 + x2) * UGA(x1 + x3)**2 + (1 - (x1 + x2)) * UgA(x1 + x3)**2) +
@@ -76,6 +72,7 @@ def equation_motion_GA_share(x1, x2, x3, UGA, UgA, T, R, P, S):
     x1_dot = (numerator / total_offspring(x1, x2, x3, UGA, UgA, T, R, P, S)) - x1
     return x1_dot
 
+
 def equation_motion_Ga_share(x1, x2, x3, UGA, UgA, T, R, P, S):
     numerator = (
                  x1 * UGA(x1 + x3)**2 * (0) +
@@ -114,7 +111,6 @@ def equation_motion_Ga_share(x1, x2, x3, UGA, UgA, T, R, P, S):
 
     x2_dot = (numerator / total_offspring(x1, x2, x3, UGA, UgA, T, R, P, S)) - x2
     return x2_dot
-
 
 
 def equation_motion_gA_share(x1, x2, x3, UGA, UgA, T, R, P, S):
@@ -195,26 +191,51 @@ def equation_motion_ga_share(x1, x2, x3, UGA, UgA, T, R, P, S):
     x4_dot = (numerator / total_offspring(x1, x2, x3, UGA, UgA, T, R, P, S)) - (1 - x1 - x2 - x3)
     return x4_dot
 
+# define symbolic variables
+x1, x2, x3 = sym.symbols('x1, x2, x3')
+T, R, P, S = sym.symbols('T, R, P, S')
 
+# equations for generalized sexual selection model
 F = sym.Matrix([equation_motion_GA_share(x1, x2, x3, UGA, UgA, T, R, P, S),
                 equation_motion_Ga_share(x1, x2, x3, UGA, UgA, T, R, P, S),
-                equation_motion_gA_share(x1, x2, x3, UGA, UgA, T, R, P, S),
-                equation_motion_ga_share(x1, x2, x3, UGA, UgA, T, R, P, S)])
+                equation_motion_gA_share(x1, x2, x3, UGA, UgA, T, R, P, S)])
+F_jac = F.jacobian((x1, x2, x3))
 
 _F = sym.lambdify((x1, x2, x3, UGA, UgA, T, R, P, S), F, modules="numpy")
-
+_F_jac = sym.lambdify((x1, x2, x3, UGA, UgA, T, R, P, S), F_jac, modules="numpy")
 
 # equations for the monomorphic_gamma model
-f = sym.Matrix([equation_motion_GA_share(x1, x2, 0, UGA, UgA, T, R, P, S),
-                equation_motion_Ga_share(x1, x2, 0, UGA, UgA, T, R, P, S)])
-_f = sym.lambdify((x1, x2, UGA, UgA, T, R, P, S), f, modules="numpy")
+f = sym.Matrix([equation_motion_GA_share(x1, 1-x1, 0, UGA, UgA, T, R, P, S)])
+f_jac = sym.diff(f, x1, 1)
 
+_f = sym.lambdify((x1, UGA, UgA, T, R, P, S), f, modules="numpy")
+_f_jac = sym.lambdify((x1, UGA, UgA, T, R, P, S), f_jac, modules="numpy")
+
+# equations for computing optimal fitness
+UGA_star, UgA_star, xG_star = sym.symbols("UGA_star, UgA_star, xG_star")
+equilibrium_values = {UGA(x1+x3): UGA_star, UgA(x1+x3): UgA_star, x1+x2: xG_star}
+N = total_offspring(x1, x2 , x3, UGA, UgA, T, R, P, S)
+equilibrium_total_offspring = N.subs(equilibrium_values)
+
+_equilibrium_total_offspring = sym.lambdify((xG_star, UGA_star, UgA_star, T, R, P, S),
+                                            equilibrium_total_offspring,
+                                            modules="numpy")
 
 def generalized_sexual_selection(x, UGA, UgA, payoff_kernel):
     ((R, S), (T, P)) = payoff_kernel
     return _F(x[0], x[1], x[2], UGA, UgA, T, R, P, S)
 
 
+def generalized_sexual_selection_jac(x, UGA, UgA, payoff_kernel):
+    ((R, S), (T, P)) = payoff_kernel
+    return _F_jac(x[0], x[1], x[2], UGA, UgA, T, R, P, S)
+
+
 def monomorphic_gamma_sexual_selection(x, UGA, UgA, payoff_kernel):
     ((R, S), (T, P)) = payoff_kernel
-    return _f(x[0], x[1], UGA, UgA, T, R, P, S)
+    return _f(x[0], UGA, UgA, T, R, P, S)
+
+
+def monomorphic_gamma_sexual_selection_jac(x, UGA, UgA, payoff_kernel):
+    ((R, S), (T, P)) = payoff_kernel
+    return _f_jac(x[0], UGA, UgA, T, R, P, S)
